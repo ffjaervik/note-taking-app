91625942d1d4f9dbc687f9d876315351594d6de9
ffjaervik <100479956+ffjaervik@users.noreply.github.com>
2022-11-18 11:04:16 +0100

    setup and start of NewNote & NoteForm component

---
+++ src/NewNote.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import NoteForm from './NoteForm'
+
+const NewNote = () => {
+  return (
+    <>
+    <h1 className='mb-4'>Hi</h1>
+    <NoteForm/>
+    </>
+  )
+}
+
+export default NewNote
