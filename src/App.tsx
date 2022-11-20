e3c91e2aa53485278825e86a5b488d9855a506c6
ffjaervik <100479956+ffjaervik@users.noreply.github.com>
2022-11-20 12:09:58 +0100

    tag-creation logic

--- src/App.tsx
+++ src/App.tsx
@@ -4,54 +4,74 @@ import { Container } from "react-bootstrap";
 import { Routes, Route, Navigate } from "react-router-dom";
 import NewNote from "./NewNote";
 import { useLocalStorage } from "./useLocalStorage";
-import {v4 as uuidV4} from 'uuid'
+import { v4 as uuidV4 } from "uuid";
 
-export type Note ={
-  id:string
+export type Note = {
+  id: string;
 } & NoteData;
 
-export type RawNote ={
-  id: string
-} & RawNoteData
+export type RawNote = {
+  id: string;
+} & RawNoteData;
 
 export type RawNoteData = {
-  title: string
-  markdown: string
+  title: string;
+  markdown: string;
   tagIds: string[];
-}
+};
 
 export type NoteData = {
-  title: string
-  markdown: string
+  title: string;
+  markdown: string;
   tags: Tag[];
-}
+};
 
 export type Tag = {
-  id: string
-  label: string
-}
+  id: string;
+  label: string;
+};
 
 function App() {
-  const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", [])
-  const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", [])
+  const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", []);
+  const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", []);
 
-  const noteWithTags = useMemo(() =>{
-    return notes.map(note => {
-      return { ...note, tags: tags.filter(tag => note.tagIds.includes(tag.id)) }
-      })
-    }, [notes,tags])
+  const noteWithTags = useMemo(() => {
+    return notes.map((note) => {
+      return {
+        ...note,
+        tags: tags.filter((tag) => note.tagIds.includes(tag.id)),
+      };
+    });
+  }, [notes, tags]);
 
-    const onCreateNote = ({tags, ...data}: NoteData) => { //Saves note inside of the notes array 
-        setNotes(prevNotes =>{
-          return [...prevNotes, {...data, id: uuidV4(), tagIds: tags.map(tag => tag.id)}]
-        })
-    }
+  const onCreateNote = ({ tags, ...data }: NoteData) => {
+    //Saves note inside of the notes array
+    setNotes((prevNotes) => {
+      return [
+        ...prevNotes,
+        { ...data, id: uuidV4(), tagIds: tags.map((tag) => tag.id) },
+      ];
+    });
+  };
+
+  const addTag = (tag: Tag) => {
+    setTags((prev) => [...prev, tag]);
+  };
 
   return (
     <Container className="my-4">
       <Routes>
         <Route path="/" element={<h1> Hi!</h1>} />
-        <Route path="/new" element={<NewNote onSubmit={onCreateNote} />} />
+        <Route
+          path="/new"
+          element={
+            <NewNote
+              onSubmit={onCreateNote}
+              onAddTag={addTag}
+              availableTags={tags}
+            />
+          }
+        />
         <Route path="/:id">
           <Route index element={<h1> Show!</h1>} />
           <Route path="edit" element={<h1> Edit!</h1>} />
