11897ccb05564c3f4c2cc7a533dca85e47c20f17
ffjaervik <100479956+ffjaervik@users.noreply.github.com>
2022-11-21 17:01:50 +0100

    filter through tag functionality complete

--- src/NoteList.tsx
+++ src/NoteList.tsx
@@ -1,18 +1,37 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 import { Row, Col, Stack, Button, Form } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import ReactSelect from "react-select";
-import { Tag } from "./App";
+import { Note, Tag } from "./App";
 
-type NoteListProps={
-    availableTags: Tag[]
-}
+type NoteListProps = {
+  availableTags: Tag[];
+  notes: Note[];
+};
 
-const NoteList = ({availableTags}: NoteListProps) => {
-    const [selectedTags, setSelectedTags] = useState<Tag[]>([])
+const NoteList = ({ availableTags, notes }: NoteListProps) => {
+  const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
+  const [title, setTitle] = useState("");
+
+  const filteredNotes = useMemo(() => { //Loops through all ofthe selected tags and make sure that everyone returns true for the statement: "check our notes if it contains the tags we are looping through"
+    return notes.filter((note) => {
+      return (
+        (title === "" ||
+          note.title.toLowerCase().includes(title.toLowerCase())) &&
+        (selectedTags.length === 0 ||
+          selectedTags.every((tag) =>
+            note.tags.some((noteTag) => noteTag.id === tag.id)
+          ))
+      );
+    });
+  }, [title, selectedTags, notes]);
+
+
+
+
   return (
     <>
-      <Row>
+      <Row className="align-items-center mb-4">
         <Col>
           <h1>Notes</h1>
         </Col>
@@ -28,35 +47,50 @@ const NoteList = ({availableTags}: NoteListProps) => {
       <Form>
         <Row className="mb-4"></Row>
         <Col>
-        <Form.Group controlId="title">
+          <Form.Group controlId="title">
             <Form.Label>Title</Form.Label>
-            <Form.Control type="text"/>
-        </Form.Group>
+            <Form.Control
+              type="text"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </Form.Group>
         </Col>
         <Col>
-        <Form.Group controlId="tags">
-              <Form.Label>Tags</Form.Label>
-              <ReactSelect
-                
-                value={selectedTags.map((tag) => {
-                  return { label: tag.label, value: tag.id };
-                })}
-                options={availableTags.map((tag) => {
-                  return { label: tag.label, value: tag.id };
-                })}
-                onChange={(tags) => {
-                  setSelectedTags(
-                    tags.map((tag) => {
-                      return { label: tag.label, id: tag.value };
-                    })
-                  );
-                }}
-                isMulti
-              />
-            </Form.Group></Col>
+          <Form.Group controlId="tags">
+            <Form.Label>Tags</Form.Label>
+            <ReactSelect
+              value={selectedTags.map((tag) => {
+                return { label: tag.label, value: tag.id };
+              })}
+              options={availableTags.map((tag) => {
+                return { label: tag.label, value: tag.id };
+              })}
+              onChange={(tags) => {
+                setSelectedTags(
+                  tags.map((tag) => {
+                    return { label: tag.label, id: tag.value };
+                  })
+                );
+              }}
+              isMulti
+            />
+          </Form.Group>
+        </Col>
       </Form>
+      <Row xs={1} sm={2} lg={3} xl={4} className="g-3">
+        {filteredNotes.map((note) => (
+          <Col key={note.id}>
+            <NoteCard id={note.id} title={note.title} tags={note.tags}  />
+          </Col>
+        ))}
+      </Row>
     </>
   );
 };
+
+
+
+
 
 export default NoteList;
