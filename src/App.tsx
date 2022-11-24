bb490325ef90b3a7f6afb2a65eb9b3915bd52c69
ffjaervik <100479956+ffjaervik@users.noreply.github.com>
2022-11-24 12:26:34 +0000

    Note Show Page complete

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { useLocalStorage } from "./useLocalStorage";
 import { v4 as uuidV4 } from "uuid";
 import NoteList from "./NoteList";
 import NoteLayout from "./NoteLayout";
+import Note from "./Note";
 
 export type Note = {
   id: string;
@@ -77,8 +78,8 @@ function App() {
             />
           }
         />
-        <Route path="/:id">
-          <Route index element={<NoteLayout notes={notesWithTags} />} />
+        <Route path="/:id" element={<NoteLayout notes={notesWithTags}/>}>
+          <Route index element={<Note/>} />
           <Route path="edit" element={<h1> Edit!</h1>} />
         </Route>
 
